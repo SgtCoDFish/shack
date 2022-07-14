b15f34568af3d5f70f31e38873e6ee6a8cd8740b
Ashley Davis <ashley.davis@jetstack.io>
2022-07-14 16:32:44 +0100

    simplify cluster.tf, add puller to namespace

--- infrastructure/terraform/cluster.tf
+++ infrastructure/terraform/cluster.tf
@@ -7,8 +7,15 @@ variable "project_id" {
   type = string
 }
 
+resource "google_service_account" "shack_sa" {
+  project = var.project_id
+  account_id   = "shack-cluster-sa"
+  display_name = "Shack Cluster SA"
+}
+
 resource "google_container_cluster" "shack_cluster" {
-  project  = var.project_id
+  project = var.project_id
+
   name     = "shack-cluster"
   location = var.gke_location
 
@@ -21,44 +28,8 @@ resource "google_container_cluster" "shack_cluster" {
 
   description = "GKE cluster for testing shack"
 
-  monitoring_service = "monitoring.googleapis.com/kubernetes"
-  logging_service    = "logging.googleapis.com/kubernetes"
-
   release_channel {
     channel = "RAPID"
-  }
-
-  maintenance_policy {
-    daily_maintenance_window {
-      start_time = "06:00"
-    }
-  }
-
-  master_auth {
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-
-  network_policy {
-    enabled  = true
-    provider = "CALICO"
-  }
-
-  enable_legacy_abac = false
-
-  addons_config {
-    http_load_balancing {
-      disabled = true
-    }
-
-    horizontal_pod_autoscaling {
-      disabled = false
-    }
-  }
-
-  lifecycle {
-    #prevent_destroy = "true"
   }
 }
 
@@ -82,25 +53,19 @@ resource "google_container_node_pool" "shack_nodepool" {
   }
 
   node_config {
-    machine_type = "e2-standard-2"
+    machine_type = "e2-medium"
 
     disk_size_gb = "35"
     disk_type    = "pd-ssd"
 
     image_type = "COS_CONTAINERD"
 
-    metadata = {
-      "disable-legacy-endpoints" = "true"
-      serial-port-enable         = true
-    }
-
     preemptible = true
 
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.shack_sa.email
     oauth_scopes = [
-      "storage-ro",
-      "logging-write",
-      "monitoring",
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 }
-
